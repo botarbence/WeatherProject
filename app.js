ef44e29689092811279823f0e2caa81533769a78
botarbence <botarbence@users.noreply.github.com>
2020-05-08 18:33:58 +0200

    1.0.1

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+  
+})
+
+app.post("/", function(req, res){
+    
+    const query = req.body.cityName;
+    const apiKey = "c0570f593463e46e4a8f4fbb4283b62a"
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = Math.floor(weatherData.main.temp);
+            const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celsius</h1>");
+            res.write("<p>The weather is currently " + description + ".</p>")
+            res.write("<img src=" + imageURL + ">")
+            res.send();
+        })
+    })
+})
+
+
+
+
+app.post("/", function(req, res){
+})
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running");
+})
